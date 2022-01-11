6f0f12c5ae48dda560e0c98a57142f06bc3dfa8e
Curtis <89085418+curtis9662@users.noreply.github.com>
2022-01-11 17:34:45 -0500

    Add files via upload

---
+++ Azure Cldy Credentialing.ps1
@@ -0,0 +1,9 @@
+﻿#Running each command below separately may be the most efficient approach
+#Azure Cldy Credentialing CJONES
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery –Force #Installs Pshell Azure Module (if user does not already use)
+Connect-AzAccount #Connect the Azure user 
+Start-Sleep 5 #Wait 5 Seconds
+Get-AzEnrollmentAccount #Will Show if EA Subscription
+Get-AzTenant #ID, Name, Category, Domains
+Get-AzSubscription #Name, Id, TenantId, State
+Get-AzResourceGroup #Lists Resource Groups
---
+++ CLDY Enablement_scripting.ps1
@@ -0,0 +1,2 @@
+﻿BackgroundColor: -#Remove Curl brackets before pasting from New-AzStorageAccount
+New-AzStorageAccount -ResourceGroupName {"resourcegroupname"} -Name {"storageaccountname"} -Location {"westus"} -SkuName {"Standard_LRS"} #Provision Storage Acct's
